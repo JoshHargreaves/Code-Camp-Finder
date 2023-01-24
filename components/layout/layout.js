84be6057e004d9583d5ad45539f1cba067168b40
Joshua Hargreaves <me@joshhargreaves.co.uk>
2023-01-24 21:51:40 +0000

    Fix build error

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -3,13 +3,16 @@ import Hero from '../hero'
 import CampContainer from '../camp-container'
 
 // @ts-ignore
-export default function Layout() {
+export default function Layout({ children }) {
     return (
     <>
         <Navbar></Navbar>
         <div className="container mx-auto">
         <Hero></Hero>
         <CampContainer></CampContainer>
+            <main className="flex-1">
+               {children}
+            </main>
         </div>
         <div>
         </div>
