c41c8bfe268fc6ac5f507d5495c5eba4ba79d0c7
Joshua Hargreaves <me@joshhargreaves.co.uk>
2023-01-24 21:27:06 +0000

    Desktop spacing

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className="flex h-screen mb-12">
+    <div className="flex h-screen justify-center mb-12">
       <div className="z-[2] mt-10" />
       <div className="p-5 z-[2]  ">
         <h2 className="text-5xl text-custom-primary font-bold"> Start your journey into computer coding</h2>
@@ -10,7 +10,7 @@ const Hero = () => {
           Find the best coding bootcamps suited to your needs !
         </p>
         <form className="flex items-center">   
-        <label for="camp-search" className="sr-only">Search camps, keywords, locations</label>
+        <label className="sr-only">Search camps, keywords, locations</label>
         <div className="relative w-full">
             <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                 <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const Navbar = () => {
   return (
     <div className="w-full z-10 ease-in duration-200">
-      <div className="max-w-[1240px] m-auto flex justify-between items-center p-4 ">
+      <div className="max-w-[1240px] flex  p-4 ">
         <Link href="/">
           <h1 className="font-bold text-3xl  text-custom-primary ">CODEcamp Finder</h1>
         </Link>
